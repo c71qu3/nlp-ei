99220f8352adc86680f932098b66ed01fb9bda77
Lukas  Kobler <lukas.kobler@hotmail.com>
2025-11-25 11:21:00 +0100

    Made metric utility function return dictionaries containing precision recall and fscore

--- utils/metrics.py
+++ utils/metrics.py
@@ -8,19 +8,31 @@ def calculate_bert_score(candidate:str, reference:str):
         
     scorer = BERTScorer(model_type='bert-base-uncased')
     P, R, F1 = scorer.score([candidate], [reference])
-    return P,R, F1
+    return {
+        "bertscore_precision": float(P[0]),
+        "bertscore_recall": float(R[0]),
+        "bertscore_f1": float(F1[0])
+    }
 
 def calculate_rouge_score(candidate:str, reference:str):
 
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
     scores = scorer.score(candidate,
                         reference)
-    return scores
+    result = {}
+    for score_name, score_v in scores.items():
+        result[f"{score_name}_precision"] = score_v.precision
+        result[f"{score_name}_recall"] = score_v.recall
+        result[f"{score_name}_fmeasure"] = score_v.fmeasure
+    return result
 
 
 def evaluate_summary(candidate:str, reference:str):
-    P, R, F1  = calculate_bert_score(candidate=candidate, reference=reference)
-    print(f"Summary reached a bert F1 score of {F1}")
 
+    bert_scores  = calculate_bert_score(candidate=candidate, reference=reference)
     rouge_scores = calculate_rouge_score(candidate=candidate, reference=reference)
-    print(f"Summary reached a rouge score of {rouge_scores}")
+    result = {}
+
+    result.update(bert_scores)
+    result.update(rouge_scores)
+    return result
